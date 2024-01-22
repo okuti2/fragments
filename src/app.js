60e2315b90d17d6551a28526ddeaec30f0e2346f
Olutoyosi Kuti <okuti2@myseneca.ca>
2024-01-21 23:30:45 -0500

    Lab 1 feedback: env variables displayed only once on startup using logger

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const helmet = require('helmet');
 const compression = require('compression');
 const passport = require('passport');
 
+if (process.env.LOG_LEVEL === 'debug') {
+  logger.debug(process.env);
+}
+
 // author and version from our package.json file
 // TODO: make sure you have updated your name in the `author` section
 //const { author, version } = require('../package.json');
